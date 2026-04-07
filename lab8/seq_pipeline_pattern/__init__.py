9d83aab8a351268860141e5d39e30bf54f8714a1
Indika Kumara <ipkumarawd@yahoo.com>
2026-04-07 18:08:53 +0200

    Updated Lab 8 Content

--- lab8/seq_pipeline_pattern/__init__.py
+++
@@ -1,3 +0,0 @@
-__all__ = ["root_agent"]
-
-from seq_agent.agent import root_agent
--- lab8/seq_pipeline_pattern/seq_agent/seqagent.py
+++ lab8/seq_pipeline_pattern/seq_agent/seqagent.py
@@ -4,7 +4,9 @@ from seq_agent.seq_exe_agent import agent2
 from seq_agent.text_to_sql_agent import agent1
 
 # Orchestrate the Pipeline
-root_agent = SequentialAgent(
-    name="BiqQueryPipeline",
+seq_agent = SequentialAgent(
+    name="BiqQueryPipelineAgent",
     sub_agents=[agent1, agent2]
 )
+
+root_agent = seq_agent
